36dec26c2106a2d1f862df5581e95799c3cde106
RunItBack1127 <thewikiscience@gmail.com>
2022-09-30 22:08:42 -0400

    Appending WSIG production server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const PROTON_APP = express();
+const SERVER_PORT = 8080;
+
+// Middleware for serving Vite static
+// build files
+PROTON_APP.use('/', express.static(path.join(__dirname, '/dist')));
+
+PROTON_APP.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+PROTON_APP.listen( SERVER_PORT || process.env.PORT );
