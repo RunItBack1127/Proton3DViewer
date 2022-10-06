331ce87f1b882d13bba1637176c5066b06ce01b8
RunItBack1127 <thewikiscience@gmail.com>
2022-10-06 00:34:29 -0400

    Implemented site intro content

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,95 @@ import SiteHeader from './components/SiteHeader.vue';
 
 <template>
     <SiteHeader />
+    <div class="modelContainer"></div>
+    <main>
+        <div class="introMessageContainer">
+            <h1>Proton, a minimal 3D viewer.</h1>
+            <h2>A 3D model viewer designed to get out of your way!</h2>
+            <h3>No cookies. No sign up required.</h3>
+        </div>
+        <div class="uploadContainer">
+            <label for="modelFileInput">Upload 3D model</label>
+        </div>
+        <footer>
+            <p>v1.0</p>
+        </footer>
+    </main>
 </template>
 
 <style scoped lang="scss">
+main {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    height: calc(100vh - 100px);
 
+    .modelContainer {
+        display: none;
+        width: 100%;
+        height: calc(100vh - 100px);
+    }
+
+    .introMessageContainer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        h1 {
+            font-size: 3rem;
+            line-height: 3rem;
+            margin-bottom: 20px;
+            text-align: center;
+        }
+
+        h2 {
+            font-size: 2rem;
+            font-weight: 200;
+            margin-bottom: 5px;
+            text-align: center;
+        }
+
+        h3 {
+            font-size: 1.5rem;
+            font-weight: 200;
+            text-align: center;
+        }
+    }
+
+    .uploadContainer {
+
+        width: 100%;
+        height: 65px;
+        max-width: 600px;
+        margin-top: 80px;
+
+        label {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+            cursor: pointer;
+            background: #000;
+            color: #fff;
+            text-align: center;
+            font-size: 1.35rem;
+            font-weight: 300;
+            border-radius: 10px;
+        }
+    }
+
+    footer {
+        position: fixed;
+        left: 50px;
+        bottom: 30px;
+
+        p {
+            font-weight: 200;
+        }
+    }
+}
 </style>
