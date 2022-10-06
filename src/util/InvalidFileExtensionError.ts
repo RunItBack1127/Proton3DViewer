818e7624eb94d354a5bb39761571d12e9569a4f0
RunItBack1127 <thewikiscience@gmail.com>
2022-10-06 18:56:10 -0400

    Extending error with invalid message

--- src/util/InvalidFileExtensionError.ts
+++ src/util/InvalidFileExtensionError.ts
@@ -1,1 +1,7 @@
-export class InvalidFileExtensionError extends Error {};
+const INVALID_FILE_ERROR_MESSAGE = "Proton does not currently support files with this extension.";
+
+export class InvalidFileExtensionError extends Error {
+    constructor() {
+        super( INVALID_FILE_ERROR_MESSAGE );
+    }
+};
