fc53a570a64a4ec9d42629471a8c0869080cfe68
RunItBack1127 <thewikiscience@gmail.com>
2022-10-10 01:15:45 -0400

    Implementing model stats component

---
+++ src/components/ModelStats.vue
@@ -0,0 +1,51 @@
+<template>
+    <aside>
+        <section>
+            <div class="infoContainer">
+                <p>File size</p>
+                <p>{{ fileSize }}</p>
+            </div>
+            <div class="infoContainer">
+                <p>Load time</p>
+                <p>{{ uploadTime }}</p>
+            </div>
+            <div class="infoContainer">
+                <p>Vertices</p>
+                <p>{{ numVertices }}</p>
+            </div>
+            <div class="infoContainer">
+                <p>Triangles</p>
+                <p>{{ numTriangles }}</p>
+            </div>
+            <button @click="closeStats">soisjs</button>
+        </section>
+    </aside>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import { useStatsStore } from '../store/stats';
+
+export default defineComponent({
+    name: 'ModelStats',
+    data() {
+        const statsStore = useStatsStore();
+        
+        return {
+            numVertices: computed(() => statsStore.numVertices),
+            numTriangles: computed(() => statsStore.numTriangles),
+            fileSize: -1,
+            uploadTime: -1
+        }
+    },
+    methods: {
+        closeStats() {
+
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
