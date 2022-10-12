96de1fbc957eae93a4f962511c66c1d1452a7eed
RunItBack1127 <thewikiscience@gmail.com>
2022-10-12 05:39:55 -0400

    Appending Express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const PROTON_APP = express();
+const SERVER_PORT = 8080 || process.env.PORT;
+
+// Middleware for serving Vite static
+// build files
+PROTON_APP.use(express.static(path.join(__dirname, 'dist')));
+
+PROTON_APP.get('/', (req, res) => {
+    res.sendFile("index.html");
+});
+
+PROTON_APP.listen( SERVER_PORT );
