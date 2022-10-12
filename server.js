ceea00f898e99fbdf014ed307aa18f05cdc92ee5
RunItBack1127 <thewikiscience@gmail.com>
2022-10-12 05:30:31 -0400

    Refactoring server

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const PROTON_APP = express();
-const APP_PORT = 8080 || process.env.PORT;
+const SERVER_PORT = 8080 || process.env.PORT;
 
-PROTON_APP.use(express.static(path.join(__dirname, "dist")));
+PROTON_APP.use(express.static(path.join(__dirname, 'dist')));
 
-PROTON_APP.get("/", (req, res) => {
+PROTON_APP.get('/', (req, res) => {
     res.sendFile("index.html");
 });
 
-PROTON_APP.listen(APP_PORT);
+PROTON_APP.listen( SERVER_PORT );
